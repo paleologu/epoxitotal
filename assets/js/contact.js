173be423d07dc4ae3d608fe2cb4e814cd5d67146
Mihail Paleologu <costin@costins-MacBook-Air.local>
2021-02-17 20:28:59 +0200

    Jekyll contact

---
+++ assets/js/contact.js
@@ -0,0 +1,35 @@
+
+
+function setupForm() {
+  var contactFormHost = 'epoxitotal-contact-form.herokuapp.com',
+      form = $('#contact-form'),
+      notice = form.find('#notice');
+
+  if (form.length) {
+    form.submit(function(ev){
+      ev.preventDefault();
+
+      $.ajax({
+        type: 'POST',
+        url: contactFormHost + 'send_email',
+        data: form.serialize(),
+        dataType: 'json',
+        success: function(response) {
+          switch (response.message) {
+            case 'success':
+              form.fadeOut(function() {
+                form.html('<h4>' + form.data('success') + '</h4>').fadeIn();
+              });
+              break;
+
+            case 'failure_email':
+              notice.text(notice.data('error')).fadeIn();
+          }
+        },
+        error: function(xhr, ajaxOptions, thrownError) {
+          notice.text(notice.data('error')).fadeIn();
+        }
+      });
+    });
+  }
+}
