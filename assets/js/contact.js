425878a0af67de9a6c356394fcbfb617c7c7cde8
Mihail Paleologu <costin@costins-MacBook-Air.local>
2021-02-23 17:42:52 +0200

    Commit

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,43 +2,43 @@
 
 
 
-  var contactFormHost = 'https://epoxitotalapi.herokuapp.com/',
-      form = $('#contact-form'),
-      notice = form.find('#notice');
+var contactFormHost = 'https://epoxitotalapi.herokuapp.com/',
+form = $('#contact-form'),
+notice = form.find('#notice');
 
 
 
-      console.log("Bla");
+console.log("Bla");
 
 
+var btn = document.querySelector('#contactFormSubmit');
+form.submit(function(ev){
+  ev.preventDefault();
+  btn.classList.add('disabled');
+  btn.setAttribute('data-original', btn.textContent);
+  btn.textContent = 'Se trimite...'; 
+  $.ajax({
+    type: 'POST',
+    url: contactFormHost + 'messages',
+    data: form.serialize(),
+    dataType: 'json',
+    success: function(response) {
+      switch (response.message) {
+        case 'success':
+        form.fadeOut(function() {
+          form.html('<h4>' + form.data('success') + '</h4>').fadeIn();
+        });
+        break;
 
+        case 'failure_email':
+        notice.text(notice.data('error')).fadeIn();
+      }
+    },
+    error: function(xhr, ajaxOptions, thrownError) {
+      notice.text(notice.data('error')).fadeIn();
+    }
+  });
+});
 
-
-    form.submit(function(ev){
-      ev.preventDefault();
-      console.log("Bli");
-      $.ajax({
-        type: 'POST',
-        url: contactFormHost + 'messages',
-        data: form.serialize(),
-        dataType: 'json',
-        success: function(response) {
-          switch (response.message) {
-            case 'success':
-              form.fadeOut(function() {
-                form.html('<h4>' + form.data('success') + '</h4>').fadeIn();
-              });
-              break;
-
-            case 'failure_email':
-              notice.text(notice.data('error')).fadeIn();
-          }
-        },
-        error: function(xhr, ajaxOptions, thrownError) {
-          notice.text(notice.data('error')).fadeIn();
-        }
-      });
-    });
-  
 
 
